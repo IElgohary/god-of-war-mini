662bf1da8c1196c21aa0990f0f7a34991ce18d55
Islam Elgohary <islam.o.elgohary@gmail.com>
2018-11-28 19:37:47 +0200

    Attacking logic

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+
+    public float range = 5.2f;
+    public float timeBetweenAttacks = 1f;
+
+    private Animator anim;
+    private GameObject player;
+    private bool playerInRange;
+    private BoxCollider[] weaponColliders;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        weaponColliders = GetComponentsInChildren<BoxCollider>();
+        player = GameManager.instance.player;
+        anim = GetComponent<Animator>();
+        StartCoroutine(attack());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Vector3.Distance(transform.position, player.transform.position) < range){
+            playerInRange = true;
+        } else {
+            playerInRange = false;
+        }
+
+	}
+
+    IEnumerator attack(){
+        if(playerInRange && !GameManager.instance.gameOver){
+            anim.Play("Basic Attack");
+            yield return new WaitForSeconds(timeBetweenAttacks);
+        }
+        yield return null;
+        StartCoroutine(attack());
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    [Tooltip("Game manages instance.")]
+    public static GameManager instance = null;
+    [Tooltip("Instance of the player.")]
+    public GameObject player;
+    [Tooltip("A boolean specifying whether the game is over.")]
+    public bool gameOver = false;
+
+    void Awake(){
+
+        if(instance == null){
+            instance = this;
+        } else if(instance != this){
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayerHit(int currentHP){
+        if(currentHP > 0) {
+            gameOver = false;
+        } else {
+            gameOver = true;
+        }
+    }
+}
