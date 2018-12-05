18d3540e18324b9b0a7140e9221ff2350fd6fb53
Karl Zakhary <karlmaged33@gmail.com>
2018-12-05 19:47:05 +0200

    Yo mofos

---
+++ Assets/RainEntertainment/FantasyMonster2/Elf Archer Pack/readMe.txt
@@ -0,0 +1,42 @@
+4 styled animated Elf Archer Pack with 23 high quality combat animations（Mecanim Ready）,perfect for action & mobile games.
+
+Mesh information: 
+VelluaArcher_A_SKEL 6242 Tris
+VelluaArcher_B_SKEL 6242 Tris
+VelluaArcher_C_SKEL 6272 Tris
+VelluaArcher_SKEL 6242 Tris
+
+Texture information:
+ElfArcher_A_DIFF 512x512
+ElfArcher_B_DIFF 512x512
+ElfArcher_C_DIFF 512x512
+ElfArcher_DIFF 512x512
+ElfArcher_WEAPON_C_DIFF 256x256
+ElfArcher_WEAPON_DIFF 256x256
+
+Animation list
+Airborne
+Airborne_Damage
+Airborne_Down
+Airborne_Splat
+Attach
+Attack1
+Attack2
+Attack3
+Attack4
+Damage
+Death
+Down
+Down_Standup
+Matinee_Attack1
+Matinee_Attack3
+Matinee_Spawn1
+Matinee_Spawn2
+Matinee_Spawn3
+Pose
+Spawn2
+Stand
+Stun
+Walk
+
+Enjoy!
---
+++ Assets/Scripts/RangedEnemyMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RangedEnemyMove : MonoBehaviour
+{
+    [Tooltip("Instance of the player.")]
+    public Transform player;
+    [Tooltip("Distance between enemy and player.")]
+    public float offset;
+
+    private NavMeshAgent nav;
+    private Animator anim;
+    private RangedEnemyHealth enemyHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+        enemyHealth = GetComponent<RangedEnemyHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameManager.instance.gameOver && enemyHealth.isAlive)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+            nav.SetDestination(player.position);
+            if (distance > offset)
+            {
+                anim.SetBool("IsWalking", true);
+                nav.enabled = true;
+            }
+            else
+            {
+                anim.SetBool("IsWalking", false);
+                nav.enabled = false;
+            }
+        }
+
+    }
+}
