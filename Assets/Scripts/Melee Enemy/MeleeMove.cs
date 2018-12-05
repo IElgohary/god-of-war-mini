c6aa981dacb536f0a3a30c92e8a9cb0e6f17b4e9
Marwan <50marwan@gmail.com>
2018-12-05 18:37:09 +0200

    Added melee enemy

---
+++ Assets/Scripts/Melee Enemy/MeleeMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeleeMove : MonoBehaviour {
+    [Tooltip("Instance of the player.")]
+    public Transform player;
+    [Tooltip("Distance between enemy and player.")]
+    public float offset;
+
+    private NavMeshAgent nav;
+    private Animator anim;
+    private MeleeHealth MeleeHealth;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+        MeleeHealth = GetComponent<MeleeHealth>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!GameManager.instance.gameOver && MeleeHealth.isAlive){
+            float distance = Vector3.Distance(player.position, transform.position);
+            nav.SetDestination(player.position);
+            if (distance > offset)
+            {
+                anim.SetBool("IsWalking", true);
+                nav.enabled = true;
+            }
+            else
+            {
+                anim.SetBool("IsWalking", false);
+                nav.enabled = false;
+            }
+        } 
+
+	}
+}
