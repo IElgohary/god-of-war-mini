f52161c81234af68eb9c3231524a11dbfa6f2360
amrrhatem <amrhatem253@gmail.com>
2018-12-05 19:59:28 +0200

    Player Run,walk

--- Assets/Fighter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Fighter/Scripts/ThirdPersonUserControl.cs
@@ -96,11 +96,11 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             }
 #if !MOBILE_INPUT
 			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 3.0f;
 #endif
 
             // pass all parameters to the character control script
-            m_Character.Move(m_Move, crouch, m_Jump);
+            m_Character.Move(0.5f*m_Move, crouch, m_Jump);
             m_Jump = false;
         }
     }
