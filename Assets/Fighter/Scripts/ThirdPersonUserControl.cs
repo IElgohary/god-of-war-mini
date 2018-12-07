dd4fa8220bbe84baf2c4b8105cabed58d85ee898
Islam Elgohary <islam.o.elgohary@gmail.com>
2018-12-07 13:13:31 +0200

    minor fixes

--- Assets/Fighter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Fighter/Scripts/ThirdPersonUserControl.cs
@@ -204,6 +204,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                     // calculate camera relative direction to move:
                     m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
                     m_Move = v * m_CamForward + h * m_Cam.right;
+
                 }
                 else
                 {
