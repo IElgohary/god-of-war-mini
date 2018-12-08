1aa9312bab5d93b83e9e9cc137854964d6a26ab9
Youssef <yoyo.hady@gmail.com>
2018-12-08 17:03:52 +0200

    Added SFX to MainMenu

---
+++ Assets/Scripts/UI/UISFX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISFX : MonoBehaviour {
+
+    public AudioSource sfx;
+
+    // Use this for initialization
+    void Start () {
+        sfx.volume = 0.3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HoverSFX(AudioClip hoverSFX)
+    {
+        
+        sfx.PlayOneShot(hoverSFX);
+    }
+
+    public void ClickSFX(AudioClip clickSFX)
+    {
+        sfx.PlayOneShot(clickSFX);
+    }
+
+    public void SlideSFX(AudioClip slideSFX)
+    {
+        sfx.PlayOneShot(slideSFX);
+
+    }
+
+}
