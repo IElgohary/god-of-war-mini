6bd34adb7dbebd4f742086bef0b6415f069a0391
Youssef <yoyo.hady@gmail.com>
2018-12-12 01:40:43 +0200

    Linked AudioSettings with the Mixer

---
+++ Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSettings : MonoBehaviour {
+
+    public AudioMixer mixer;
+    public Slider music;
+    public Slider speech;
+    public Slider fx;
+
+	// Use this for initialization
+	void Start () {
+        music.value = -5;
+        speech.value = -5;
+        fx.value = -5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mixer.SetFloat("musicVol", music.value);
+        mixer.SetFloat("speechVol", speech.value);
+        mixer.SetFloat("fxVol", fx.value);
+    }
+}
