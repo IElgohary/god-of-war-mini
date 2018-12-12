25431e8a935e47042d7b1fb1105e20b5ebe0c783
Youssef <yoyo.hady@gmail.com>
2018-12-12 02:29:53 +0200

    AudioSettings display the value

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -11,22 +11,34 @@ public class AudioSettings : MonoBehaviour {
     public Slider speech;
     public Slider fx;
 
-	// Use this for initialization
-	void Start () {
+    public TMPro.TextMeshProUGUI musicVal;
+    public TMPro.TextMeshProUGUI speechVal;
+    public TMPro.TextMeshProUGUI fxVal;
+
+    // Use this for initialization
+    void Start () {
         music.value = -5;
         speech.value = -5;
         fx.value = -5;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		mixer.SetFloat("musicVol", music.value);
-        PlayerPrefs.SetFloat("musicVol", music.value);
 
+    public void changeMusicVol() {
+        mixer.SetFloat("musicVol", music.value);
+        PlayerPrefs.SetFloat("musicVol", music.value);
+        musicVal.text = ((int)music.value).ToString() + " dB";
+    }
+
+    public void changeSpeechVol() {
         mixer.SetFloat("speechVol", speech.value);
         PlayerPrefs.SetFloat("speechVol", speech.value);
+        speechVal.text = ((int)speech.value).ToString() + " dB";
+    }
 
+    public void changeFXVol()
+    {
         mixer.SetFloat("fxVol", fx.value);
         PlayerPrefs.SetFloat("fxVol", fx.value);
+        fxVal.text = ((int)fx.value).ToString() + " dB";
     }
 }
