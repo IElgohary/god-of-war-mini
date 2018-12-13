2cc18ad58891df58c6c1fa40a67048ce23f96fe3
Youssef <yoyo.hady@gmail.com>
2018-12-13 19:48:29 +0200

    Prototyping Orbiting Camera

--- Assets/Scripts/Kratos/CameraFollow.cs
+++ Assets/Scripts/Kratos/CameraFollow.cs
@@ -30,6 +30,6 @@ public class CameraFollow : MonoBehaviour
 
         Vector3 targetCamPos = target.position + offset;
         transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
-
+        
     }
 }
---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public Transform lookAt;
+    public Transform camTransform;
+    Camera cam;
+
+    private float distance = 7.0f;
+    private float currentX = 0.0f;
+    private float currentY = 30.0f;
+    private float sensitivityX = 4.0f;
+    private float sensitivityY = 1.0f;
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0,0,-distance);
+        Quaternion rotation = Quaternion.Euler(currentY, sensitivityX*currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+
+        camTransform.LookAt(lookAt.position);
+    }
+
+}
